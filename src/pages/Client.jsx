6fcb3d39a942154c1e0c4158fcf4d28168a58d29
Daniel160407 <abulashvilidani@hotmail.com>
2025-07-31 13:33:54 +0400

    fix: fixed additional bugs

--- src/pages/Client.jsx
+++ src/pages/Client.jsx
@@ -7,7 +7,7 @@ import GroupsList from "../components/lists/GroupsList";
 import WebSocketManager from "../components/hooks/WebSocketManager";
 import ClientQuestion from "../components/model/ClientQuestion";
 import useAxios from "../components/hooks/UseAxios";
-import { ANSWER, CLIENT_ROLE, GROUP_CREATED, GROUP_CREATION, PLAYER_ANSWERED, QUESTION, QUESTION_CANCEL, QUIZ_START } from "../Constant";
+import { ANSWER, CLIENT_ROLE, GROUP_CREATED, GROUP_CREATION, QUESTION, QUESTION_CANCEL, QUIZ_START } from "../Constant";
 
 
 const Client = () => {
